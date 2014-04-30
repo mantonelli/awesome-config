c3cc54b590399bcadf3099b4b5010764cc4f0310
Matheus Antonelli <matheus.antonelli@gmail.com>
2014-04-30 15:05:28 -0300

    Novo papel de parede e criação da variável home_folder no tema

--- themes/zenburn/theme.lua
+++ themes/zenburn/theme.lua
@@ -8,7 +8,8 @@
 
 -- {{{ Main
 theme = {}
-theme.wallpaper = "/usr/share/awesome/themes/zenburn/zenburn-background.png"
+theme.home_folder = "/home/mantonelli/.config/awesome/themes/zenburn"
+theme.wallpaper   = theme.home_folder .. "/wallpaper/background.jpg"
 -- }}}
 
 -- {{{ Styles
@@ -72,54 +73,54 @@ theme.menu_width  = 100
 
 -- {{{ Icons
 -- {{{ Taglist
-theme.taglist_squares_sel   = "/usr/share/awesome/themes/zenburn/taglist/squarefz.png"
-theme.taglist_squares_unsel = "/usr/share/awesome/themes/zenburn/taglist/squarez.png"
+theme.taglist_squares_sel   = theme.home_folder .. "/taglist/squarefz.png"
+theme.taglist_squares_unsel = theme.home_folder .. "/taglist/squarez.png"
 --theme.taglist_squares_resize = "false"
 -- }}}
 
 -- {{{ Misc
-theme.awesome_icon           = "/usr/share/awesome/themes/zenburn/awesome-icon.png"
+theme.awesome_icon           = theme.home_folder .. "/awesome-icon.png"
 theme.menu_submenu_icon      = "/usr/share/awesome/themes/default/submenu.png"
 -- }}}
 
 -- {{{ Layout
-theme.layout_tile       = "/usr/share/awesome/themes/zenburn/layouts/tile.png"
-theme.layout_tileleft   = "/usr/share/awesome/themes/zenburn/layouts/tileleft.png"
-theme.layout_tilebottom = "/usr/share/awesome/themes/zenburn/layouts/tilebottom.png"
-theme.layout_tiletop    = "/usr/share/awesome/themes/zenburn/layouts/tiletop.png"
-theme.layout_fairv      = "/usr/share/awesome/themes/zenburn/layouts/fairv.png"
-theme.layout_fairh      = "/usr/share/awesome/themes/zenburn/layouts/fairh.png"
-theme.layout_spiral     = "/usr/share/awesome/themes/zenburn/layouts/spiral.png"
-theme.layout_dwindle    = "/usr/share/awesome/themes/zenburn/layouts/dwindle.png"
-theme.layout_max        = "/usr/share/awesome/themes/zenburn/layouts/max.png"
-theme.layout_fullscreen = "/usr/share/awesome/themes/zenburn/layouts/fullscreen.png"
-theme.layout_magnifier  = "/usr/share/awesome/themes/zenburn/layouts/magnifier.png"
-theme.layout_floating   = "/usr/share/awesome/themes/zenburn/layouts/floating.png"
+theme.layout_tile       = theme.home_folder .. "/layouts/tile.png"
+theme.layout_tileleft   = theme.home_folder .. "/layouts/tileleft.png"
+theme.layout_tilebottom = theme.home_folder .. "/layouts/tilebottom.png"
+theme.layout_tiletop    = theme.home_folder .. "/layouts/tiletop.png"
+theme.layout_fairv      = theme.home_folder .. "/layouts/fairv.png"
+theme.layout_fairh      = theme.home_folder .. "/layouts/fairh.png"
+theme.layout_spiral     = theme.home_folder .. "/layouts/spiral.png"
+theme.layout_dwindle    = theme.home_folder .. "/layouts/dwindle.png"
+theme.layout_max        = theme.home_folder .. "/layouts/max.png"
+theme.layout_fullscreen = theme.home_folder .. "/layouts/fullscreen.png"
+theme.layout_magnifier  = theme.home_folder .. "/layouts/magnifier.png"
+theme.layout_floating   = theme.home_folder .. "/layouts/floating.png"
 -- }}}
 
 -- {{{ Titlebar
-theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/zenburn/titlebar/close_focus.png"
-theme.titlebar_close_button_normal = "/usr/share/awesome/themes/zenburn/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = theme.home_folder .. "/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = theme.home_folder .. "/titlebar/close_normal.png"
 
-theme.titlebar_ontop_button_focus_active  = "/usr/share/awesome/themes/zenburn/titlebar/ontop_focus_active.png"
-theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/zenburn/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_inactive  = "/usr/share/awesome/themes/zenburn/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/zenburn/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_active  = theme.home_folder .. "/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active = theme.home_folder .. "/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive  = theme.home_folder .. "/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive = theme.home_folder .. "/titlebar/ontop_normal_inactive.png"
 
-theme.titlebar_sticky_button_focus_active  = "/usr/share/awesome/themes/zenburn/titlebar/sticky_focus_active.png"
-theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/zenburn/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_inactive  = "/usr/share/awesome/themes/zenburn/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/zenburn/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_active  = theme.home_folder .. "/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active = theme.home_folder .. "/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive  = theme.home_folder .. "/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive = theme.home_folder .. "/titlebar/sticky_normal_inactive.png"
 
-theme.titlebar_floating_button_focus_active  = "/usr/share/awesome/themes/zenburn/titlebar/floating_focus_active.png"
-theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/zenburn/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_inactive  = "/usr/share/awesome/themes/zenburn/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/zenburn/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_active  = theme.home_folder .. "/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_active = theme.home_folder .. "/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive  = theme.home_folder .. "/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive = theme.home_folder .. "/titlebar/floating_normal_inactive.png"
 
-theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/zenburn/titlebar/maximized_focus_active.png"
-theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/zenburn/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/zenburn/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/zenburn/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_active  = theme.home_folder .. "/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active = theme.home_folder .. "/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive  = theme.home_folder .. "/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = theme.home_folder .. "/titlebar/maximized_normal_inactive.png"
 -- }}}
 -- }}}
 
